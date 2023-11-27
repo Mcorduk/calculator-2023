31afef36d4a542a4c73db244149c3e474757b301
Mcorduk <om.corduk@gmail.com>
2023-11-27 13:48:11 +0300

    Write Numbers and operators on screen correctly, Add Working Clear Button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const operatorArray = ["÷", "x", "-","+"];
+let num1;
+let num2;
+let operator;
 
 /* Functions for the seperate operations */
 const add = (a, b) => a + b;
@@ -6,6 +8,7 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+/* Operate on given numbers and selected operator */
 const operation = (num1, num2, operator) => {
 
     switch (operator) {
@@ -22,33 +25,44 @@ const operation = (num1, num2, operator) => {
     }
 };
 
-let num1;
-let num2;
-let operator;
 /* Update operator variable each time the operator variable is clicked */
 const operatorButtons = document.querySelectorAll('.operation');
 operatorButtons.forEach(button => {
     button.addEventListener('click', e => {
         operator = e.target.innerText;
-        writeToScreen(operator)
-        
+        writeToDisplay(operator);
     })
 });
 
+/* Write Numbers to display and update num1,num2 variables */
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach(button => {
     button.addEventListener('click', e => {
         num1 = e.target.innerText;
+        writeToDisplay(num1)
     })
 });
 
-const writeToScreen = (item) => {
+/* General Function needed to write characters on calc display*/
+const writeToDisplay = (item) => {
+    const operatorArray = ["÷", "x", "-", "+"];
     let currentDisplay = document.querySelector("span").innerText;
     const length = currentDisplay.length;
     const lastItemAtDisplay = currentDisplay[length - 1]
-    if ((operatorArray.includes(item)) 
-    && (operatorArray.includes(lastItemAtDisplay))){
+
+    /*Delete the previous operator 
+    if there is one so no multiple operators exist */
+    if ((operatorArray.includes(item))
+        && (operatorArray.includes(lastItemAtDisplay))) {
         currentDisplay = currentDisplay.slice(0, -1);
     }
-     currentDisplay += item;
+    /*Append new items */
+    currentDisplay += item;
+    /* Update the span element with new Display */
+    document.querySelector("span").innerText = currentDisplay;
 }
+
+/* Clear Display when clear button is clicked */
+const clearDisplay = () => document.querySelector("span").innerText = "";
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener('click', clearDisplay);
